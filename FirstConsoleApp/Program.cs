6a76d33195398b6fa163919d396eb6b15bc6c2e3
salehin <salehin.anjum@gmail.com>
2020-06-22 20:55:43 +0530

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 20.0;
+            double y = 34.987;
+            double z = x + y;
+            Console.WriteLine("Sum is : " + z);
+            //Console.WriteLine("Hellow World");
+            //Console.ReadLine();
+        }
+    }
+}
